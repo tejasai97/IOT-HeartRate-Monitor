7b005089d514107c033599569e0956dbd2e18c5c
Gadipally Sai Teja <teja.g14@gmail.com>
2017-04-16 17:34:10 +0530

    Add files via upload
    This code is for sending data to thingspeak using nodemcu

---
+++ NodeMcu.ino
@@ -0,0 +1,156 @@
+#include <ESP8266WiFi.h>
+
+#include <ESP8266HTTPClient.h>
+
+WiFiClient client;
+
+String thingSpeakAddress= "http://api.thingspeak.com/update?";
+String writeAPIKey;
+String tsfield1Name;
+String request_string;
+
+HTTPClient http;
+const int AnalogIn  = A0;
+
+int readingIn = 0;
+
+float temp;
+int hum;
+String tempC;
+int error;
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 13;                // pin to blink led at each beat
+int fadePin = 5;
+int fadeRate = 0;
+
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded! 
+volatile boolean Pulse = false;     // "True" when heartbeat is detected. "False" when not a "live beat". 
+volatile boolean QS = false;        // becomes true when Arduino finds a beat.
+
+// Regards Serial OutPut  -- Set This Up to your needs
+static boolean serialVisual = true;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse 
+volatile int rate[10];                    // array to hold last ten IBI values
+volatile unsigned long sampleCounter = 0;          // used to determine pulse timing
+volatile unsigned long lastBeatTime = 0;           // used to find IBI
+volatile int P =512;                      // used to find peak in pulse wave, seeded
+volatile int T = 512;                     // used to find trough in pulse wave, seeded
+volatile int thresh = 525;                // used to find instant moment of heart beat, seeded
+volatile int amp = 100;                   // used to hold amplitude of pulse waveform, seeded
+volatile boolean firstBeat = true;        // used to seed rate array so we startup with reasonable BPM
+volatile boolean secondBeat = false;      // used to seed rate array so we startup with reasonable BPM
+
+
+void setup()
+{
+  Serial.begin(9600);
+    WiFi.disconnect();
+   WiFi.begin("ASUS","tejahy1313");
+  while ((!(WiFi.status() == WL_CONNECTED))){
+    delay(300);
+
+  }
+
+}
+
+
+void loop()
+{
+    readingIn = analogRead(pulsePin);//(AnalogIn);
+    Serial.println(BPM);
+    if (client.connect("api.thingspeak.com",80)) {
+      writeAPIKey = "key=JYLKR4WVJ5NPEPOC";
+      tsfield1Name = "&field1=10";
+      request_string = thingSpeakAddress;
+      request_string += "key=";
+      request_string += "RFE3DEVPK75V6Q21";
+      request_string += "&";
+      request_string += "field1";
+      request_string += "=";
+      ISR(readingIn);
+      request_string += /*(readingIn/12)*2;*/(random(60,120));
+      http.begin(request_string);
+      http.GET();
+      http.end();
+
+    }
+    delay(1000);
+
+}
+void ISR(int anala){                       // triggered when Timer2 counts to 124
+  //cli();                                      // disable interrupts while we do this
+  Signal = anala;//analogRead(pulsePin);              // read the Pulse Sensor 
+  sampleCounter += 2;                         // keep track of the time in mS
+  int N = sampleCounter - lastBeatTime;       // monitor the time since the last beat to avoid noise
+
+    //  find the peak and trough of the pulse wave
+  if(Signal < thresh && N > (IBI/5)*3){      // avoid dichrotic noise by waiting 3/5 of last IBI
+    if (Signal < T){                         // T is the trough
+      T = Signal;                            // keep track of lowest point in pulse wave 
+    }
+  }
+
+  if(Signal > thresh && Signal > P){        // thresh condition helps avoid noise
+    P = Signal;                             // P is the peak
+  }                                         // keep track of highest point in pulse wave
+
+  //  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
+  // signal surges up in value every time there is a pulse
+  if (N > 250){                                   // avoid high frequency noise
+    if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) ){        
+      Pulse = true;                               // set the Pulse flag when there is a pulse
+      IBI = sampleCounter - lastBeatTime;         // time between beats in mS
+      lastBeatTime = sampleCounter;               // keep track of time for next pulse
+
+      if(secondBeat){                        // if this is the second beat
+        secondBeat = false;                  // clear secondBeat flag
+        for(int i=0; i<=9; i++){             // seed the running total to get a realistic BPM at startup
+          rate[i] = IBI;                      
+        }
+      }
+
+      if(firstBeat){                         // if it's the first time beat is found
+        firstBeat = false;                   // clear firstBeat flag
+        secondBeat = true;                   // set the second beat flag
+       /* sei();                               // enable interrupts again
+        return; */                             // IBI value is unreliable so discard it
+      }   
+      word runningTotal = 0;                  // clear the runningTotal variable    
+
+      for(int i=0; i<=8; i++){                // shift data in the rate array
+        rate[i] = rate[i+1];                  // and drop the oldest IBI value 
+        runningTotal += rate[i];              // add up the 9 oldest IBI values
+      }
+
+      rate[9] = IBI;                          // add the latest IBI to the rate array
+      runningTotal += rate[9];                // add the latest IBI to runningTotal
+      runningTotal /= 10;                     // average the last 10 IBI values 
+      BPM = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
+      QS = true;                              // set Quantified Self flag 
+      // QS FLAG IS NOT CLEARED INSIDE THIS ISR
+    }                       
+  }
+
+  if (Signal < thresh && Pulse == true){   // when the values are going down, the beat is over
+    //digitalWrite(blinkPin,LOW);            // turn off pin 13 LED
+    Pulse = false;                         // reset the Pulse flag so we can do it again
+    amp = P - T;                           // get amplitude of the pulse wave
+    thresh = amp/2 + T;                    // set thresh at 50% of the amplitude
+    P = thresh;                            // reset these for next time
+    T = thresh;
+  }
+
+  if (N > 2500){                           // if 2.5 seconds go by without a beat
+    thresh = 512;                          // set thresh default
+    P = 512;                               // set P default
+    T = 512;                               // set T default
+    lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
+    firstBeat = true;                      // set these to avoid noise
+    secondBeat = false;                    // when we get the heartbeat back
+  }
+
+ // sei();     
+  // enable interrupts when youre done!
+}
